5d40e9e5d5f3d2d2de03bde2feded6e0ee93cd7c
Allan de Miranda <allandemiranda@gmail.com>
2026-02-03 19:55:28 +0100

    Initial commit

---
+++ src/main/java/br/allandemiranda/fx/robot/FxCountdownApplication.java
@@ -0,0 +1,13 @@
+package br.allandemiranda.fx.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxCountdownApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FxCountdownApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/allandemiranda/fx/robot/FxCountdownApplicationTests.java
@@ -0,0 +1,13 @@
+package br.allandemiranda.fx.robot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxCountdownApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
