113fdb75e640e0b545276ddd7e4f4e7fa6e8679b
Allan de Miranda <allandemiranda@gmail.com>
2026-02-19 21:56:11 +0100

    Initial commit

---
+++ src/main/java/br/allandemiranda/fx/robot/service/CandlestickService.java
@@ -0,0 +1,29 @@
+package br.allandemiranda.fx.robot.service;
+
+import br.allandemiranda.fx.robot.dto.CandlestickCreateDto;
+import br.allandemiranda.fx.robot.dto.CandlestickDto;
+import br.allandemiranda.fx.robot.mapper.CandlestickCreateMapper;
+import br.allandemiranda.fx.robot.mapper.CandlestickMapper;
+import br.allandemiranda.fx.robot.model.Candlestick;
+import br.allandemiranda.fx.robot.repository.CandlestickRepository;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Getter(AccessLevel.PRIVATE)
+@Service
+public class CandlestickService {
+
+  private final CandlestickRepository repository;
+  private final CandlestickMapper mapper;
+  private final CandlestickCreateMapper createMapper;
+
+  public CandlestickDto create(CandlestickCreateDto candlestickCreateDto) {
+    Candlestick entity = this.getCreateMapper().toEntity(candlestickCreateDto);
+    Candlestick candlestick = this.getRepository().save(entity);
+    return this.getMapper().toDto(candlestick);
+  }
+
+}
