de32e526e4f200f14d4802b5f1433f5ae7bb2dd2
Allan de Miranda <allandemiranda@gmail.com>
2026-02-24 22:23:20 +0100

    Initial commit

--- src/main/java/br/allandemiranda/fx/robot/controller/DashboardController.java
+++ src/main/java/br/allandemiranda/fx/robot/controller/DashboardController.java
@@ -1,13 +1,20 @@
 package br.allandemiranda.fx.robot.controller;
 
 import br.allandemiranda.fx.robot.dto.DashboardDto;
+import br.allandemiranda.fx.robot.enums.Timeframe;
+import br.allandemiranda.fx.robot.exception.DashboardNotFoundException;
 import br.allandemiranda.fx.robot.service.DashboardService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import java.util.Collection;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +31,12 @@ public class DashboardController {
   @GetMapping(produces = "application/json")
   public Collection<DashboardDto> getDashboards() {
     return this.getDashboardService().getDashboard();
+  }
+
+  @ResponseStatus(HttpStatus.OK)
+  @GetMapping(path = "/symbols/{name}/timeframes/{period}", produces = "application/json")
+  public DashboardDto getDashboard(@PathVariable @NotNull @NotEmpty @NotBlank @Valid String name, @PathVariable @NotNull @Valid Timeframe period) {
+    return this.getDashboardService().getDashboard(name, period).orElseThrow(DashboardNotFoundException::new);
   }
 
 }
---
+++ src/main/java/br/allandemiranda/fx/robot/exception/DashboardNotFoundException.java
@@ -0,0 +1,14 @@
+package br.allandemiranda.fx.robot.exception;
+
+import lombok.experimental.StandardException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+@StandardException
+public class DashboardNotFoundException extends RuntimeException {
+
+  public DashboardNotFoundException(String name) {
+    super("Dashboard not found: " + name);
+  }
+}
