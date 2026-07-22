7b89c5048d8d78186559cdf15864b3bf579d0c1b
Allan de Miranda <allandemiranda@gmail.com>
2026-07-22 21:56:07 +0200

    initial commit

--- src/test/java/br/allandemiranda/fx/robot/FxCountdownApplicationTests.java
+++ src/test/java/br/allandemiranda/fx/robot/FxCountdownApplicationTests.java
@@ -1,6 +1,9 @@
 package br.allandemiranda.fx.robot;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -17,5 +20,13 @@ class FxCountdownApplicationTests {
 
   @Test
   void contextLoads() {
+  }
+
+  @Test
+  void mainMethodRuns() {
+    try (MockedStatic<SpringApplication> springApplication = Mockito.mockStatic(SpringApplication.class)) {
+      FxCountdownApplication.main(new String[]{});
+      springApplication.verify(() -> SpringApplication.run(FxCountdownApplication.class, new String[]{}));
+    }
   }
 }
